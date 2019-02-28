62d83d51907ca52c9acf1ffea917a8a29d97080e
AlesiaSk <alesya213@gmail.com>
2019-03-01 00:48:42 +0300

    Added user's name and user's surname

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // this is an example of improting data from JSON
 import orders from '../data/orders.json';
+import users from '../data/users.json';
 
 export default (function () {
     const tableBody = document.getElementById('orders_table_body');
@@ -19,13 +20,12 @@ export default (function () {
         const tableRow = document.createElement('TR');
         tableRow.setAttribute('id', `order_${order.id}`);
         createCell(tableRow, order.transaction_id);
-        createCell(tableRow, order.user_id).className = 'user_data';
+        createCell(tableRow, order.user_id, true, order).className = 'user_data';
         createCell(tableRow, new Date(parseInt(order.created_at, 10)));
         createCell(tableRow, '$'+order.total);
         createCell(tableRow, getFormattedCardNumber(order.card_number));
-        createCell(tableRow,order.card_number);
-        createCell(tableRow, order.order_country);
-        createCell(tableRow, order.order_ip);
+        createCell(tableRow,order.card_type);
+        createCell(tableRow, `${order.order_country} (${order.order_ip})` );
         tableBody.appendChild(tableRow);
     }
 
@@ -35,11 +35,33 @@ export default (function () {
         )).join('');
     }
 
-    function createCell(tableRow, data){
+    function createCell(tableRow, data, isUserInfo, order){
         const tableData = document.createElement('TD');
-        const text = document.createTextNode(data);
+        let text;
 
-        tableData.appendChild(text);
+        if(isUserInfo){
+            users.forEach(user => {
+                if(user.id == order.user_id){
+                    const userLink = document.createElement('A');
+                    if(user.gender == 'Female'){
+                        data = `Ms. `;
+                    }
+                    else{
+                        data = `Mr. `;
+                    }
+                    data += `${user.first_name} ${user.last_name}`;
+                    text = document.createTextNode(data);
+                    userLink.appendChild(text);
+                    tableData.appendChild(userLink);
+                }
+            });
+        }
+        else{
+            text = document.createTextNode(data);
+            tableData.appendChild(text);
+        }
+
+
         tableRow.appendChild(tableData);
 
         return tableData;
